26a48b208128fdad5ed4f0aad6b7960efb1eec2f
Michael Sechooler <mikesech@gmail.com>
2014-01-26 03:28:02 +0000

    Added regular polling; filter emails locally
    This commit does two things primarily:
      * The program will not quit after the first time downloading
        and playing available messages. Instead, it will sleep for
        one minute and repeat the process ad infinitum.
      * It no longer filters the e-mails by subject through a IMAP
        search criteria. For some reason, it took a lot longer for
        new e-mails to appear when using that criteria. We get near
        instant availablity by downloading all unseen e-mails and
        filtering them out on our own.

--- green_shirt.py
+++ green_shirt.py
@@ -6,6 +6,7 @@ PASSWORD =  XXX_CONFIGURE_ME
 
 import imaplib, email, email.parser, re, httplib, urllib, subprocess, sys, quopri
 from yaml import dump
+from time import sleep
 
 def extract_text_body(msg):
   for part in msg.walk():
@@ -44,25 +45,49 @@ def speak_text(text):
   mp3 = text_to_mp3(text)
   play_mp3_from_memory(mp3)
 
-def handle_message(body):
-  print "new message: %s" % body
-  try:
-    speak_text(body)
-  except Exception as e:
-    print >> sys.stderr, "error playing message: %s" % str(e)
+def handle_message(subject, body):
+  if re.search(r'\[GREEN SHIRT\]', subject) is None:
+    print "skipping message: %s" % subject
+  else:
+    print "new message: %s" % body
+    try:
+      speak_text(body)
+    except Exception as e:
+      print >> sys.stderr, "error playing message: %s" % str(e)
 
-M = imaplib.IMAP4(IMAP_HOST)
-M.login(USERNAME, PASSWORD)M.select()
-typ, data = M.search(None, '(UNSEEN SUBJECT "GREEN SHIRT")')
-for num in data[0].split():
-  typ, data = M.fetch(num, '(RFC822)')
-  msg = email.parser.Parser().parsestr(data[0][1])
-  
-  subject = msg['subject']
-  body = extract_text_body(msg)
-  
-  if body is not None:
-    handle_message(body)
-  M.store(num, '+FLAGS', '\Seen')
-M.close()
-M.logout()
+def process_imap_results(M, data):
+  items = data[0].split()
+  print >> sys.stderr, "processing %d items" % len(items)
+  for num in items:
+    typ, data = M.fetch(num, '(RFC822)')
+    msg = email.parser.Parser().parsestr(data[0][1])
+    
+    subject = msg['subject']
+    body = extract_text_body(msg)
+    
+    if body is not None:
+      handle_message(subject, body)
+
+def main():
+  M = imaplib.IMAP4(IMAP_HOST)
+  #M = imaplib.IMAP4_SSL(IMAP_HOST)
+  M.login(USERNAME, PASSWORD)
+  M.select()
+  while True:
+#    typ, data = M.search(None, '(UNSEEN SUBJECT "GREEN SHIRT")')
+    typ, data = M.search(None, '(UNSEEN)')
+    if typ == "OK":
+      process_imap_results(M, data)
+    else:
+      print >> sys.stderr, "error searching imap"
+    
+    sleep(60)
+  M.close()
+  M.logout()
+
+
+if __name__ == "__main__":
+  try:
+    main()
+  except KeyboardInterrupt:
+    exit(0)
